0318a733b209b2f1888b9b59f49e221bc4b64e4d
Sjifra de Leeuw <39857051+sdleeuw2@users.noreply.github.com>
2019-11-29 06:23:17 +0100

    .

---
+++ Analyses/Appendices/source_file.R
@@ -0,0 +1,6 @@
+
+location_folder = "/Users/sjifradeleeuw/GitHub/" # change location
+
+data_dir = paste0(location_folder, "Replication-Code-Democratic-Support-And-Left-Right-Dimension/Data/") # do not change
+out_dir = paste0(location_folder, "Replication-Code-Democratic-Support-And-Left-Right-Dimension/") # do not change
+robust_dir = paste0(location_folder, "Replication-Code-Democratic-Support-And-Left-Right-Dimension/Output/Appendices/") # do not change
---
+++ Data Preparation/source_file.R
@@ -0,0 +1,5 @@
+
+location_folder = "/Users/sjifradeleeuw/GitHub/" # change location
+
+data_dir = paste0(location_folder, "Replication-Code-Democratic-Support-And-Left-Right-Dimension/Data/") # do not change
+out_dir = paste0(location_folder, "Replication-Code-Democratic-Support-And-Left-Right-Dimension/") # do not change
